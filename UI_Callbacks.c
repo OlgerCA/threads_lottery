fef4a89f5590f5104b0ab69c614ba837e6adce88
andresme <andres.me@gmail.com>
2016-03-03 17:20:43 -0600

    Lottery No UI

--- UI_Callbacks.c
+++ UI_Callbacks.c
@@ -4,5 +4,5 @@
 void UpdateUI(long threadID, double accuResult, double percentage, int execution) {
 //	printf("ThreadID: %ld , result: %f percentage: %f execution: %d\n", threadID, accuResult, percentage, execution);
 
-	progressbarlist_item_update(threadID, accuResult, percentage, 1);
+//	progressbarlist_item_update(threadID, accuResult, percentage, 1);
 }
--- main.c
+++ main.c
@@ -4,9 +4,9 @@
 #include "Thread_Callbacks.h"
 
 /* THIS SHOULD BE REPLACED WITH INFO FROM FILE */
-//#define PREEMPTIVE 1
-//#define MILISECONDS 100
-//#define NUM_THREADS 5
+#define PREEMPTIVE 1
+#define MILISECONDS 100
+#define NUM_THREADS 5
 
 int main (int argc, char *argv[])
 {
@@ -15,32 +15,32 @@ int main (int argc, char *argv[])
 
 
     /* Initialize GTK+ */
-    g_log_set_handler("Gtk", G_LOG_LEVEL_WARNING, (GLogFunc) gtk_false, NULL);
-    gtk_init(&argc, &argv);
-    g_log_set_handler("Gtk", G_LOG_LEVEL_WARNING, g_log_default_handler, NULL);
-
-    viewer_show();
-
-    gtk_main();
-
-    /* THIS SHOULD BE REPLACED WITH INFO FROM FILE */
-//    int i = 0;
-//    long *tickets = (long*) malloc(NUM_THREADS * sizeof(long));
-//    long *work = (long*) malloc(NUM_THREADS * sizeof(long));
-//    for(; i < NUM_THREADS; i++){
-//        tickets[i] = 10 + rand() % 10000;
-//        work[i] = 50;
-//    }
-
-    /* THIS SHOULD BE REPLACED WITH INFO FROM FILE */
-//    LoteryScheduler_Init(NUM_THREADS, runThread, PREEMPTIVE, MILISECONDS, tickets, work);
+//    g_log_set_handler("Gtk", G_LOG_LEVEL_WARNING, (GLogFunc) gtk_false, NULL);
+//    gtk_init(&argc, &argv);
+//    g_log_set_handler("Gtk", G_LOG_LEVEL_WARNING, g_log_default_handler, NULL);
 //
-//    int retVal = sigsetjmp(Scheduler->context, 1);
-//    if (retVal == 1) {
-//        return 0;
-//    }
-//    LoteryScheduler_Schedule(Scheduler);
-//    LoteryScheduler_Free(Scheduler);
+//    viewer_show();
+//
+//    gtk_main();
+
+    /* THIS SHOULD BE REPLACED WITH INFO FROM FILE */
+    int i = 0;
+    long *tickets = (long*) malloc(NUM_THREADS * sizeof(long));
+    long *work = (long*) malloc(NUM_THREADS * sizeof(long));
+    for(; i < NUM_THREADS; i++){
+        tickets[i] = 10 + rand() % 10000;
+        work[i] = 50;
+    }
+
+    /* THIS SHOULD BE REPLACED WITH INFO FROM FILE */
+    LoteryScheduler_Init(NUM_THREADS, runThread, PREEMPTIVE, MILISECONDS, tickets, work);
+
+    int retVal = sigsetjmp(Scheduler->context, 1);
+    if (retVal == 1) {
+        return 0;
+    }
+    LoteryScheduler_Schedule(Scheduler);
+    LoteryScheduler_Free(Scheduler);
 
 
     return 0;
